6ae502d7e263a1f7d1f6e7f6131e6df00bc254e3
Julian <julian@codecollective.us>
2025-07-27 21:25:14 -0400

    adds legal and drone group

--- event_sources.py
+++ event_sources.py
@@ -19,7 +19,8 @@ sources = {
         "https://www.meetup.com/pitch-labs/",
         "https://www.meetup.com/Baltimore-Hackerspace/",
         "https://www.meetup.com/maryland-red-hat-user-group/",
-        "https://www.meetup.com/columbia-politics-meetup-group/"
+        "https://www.meetup.com/columbia-politics-meetup-group/",
+        "https://www.meetup.com/dc-area-drone-user-group/"
     ],
     "Eventbrite":[
         "https://www.eventbrite.com/e/tential-tech-tuesday-tickets-1366415157519",
