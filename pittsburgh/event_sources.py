9d24da460717adcccfa11c50c79af5ac595a716c
Julian <jcloiacon@gmail.com>
2026-01-22 22:44:20 -0500

    adds pittsburgh

---
+++ pittsburgh/event_sources.py
@@ -0,0 +1,26 @@
+sources = {
+    "Meetup":[
+        #"https://www.meetup.com/baltimore-indie-game-developers-group/events/",
+    ],
+    "Eventbrite":[ #single events
+        #"https://www.eventbrite.com/e/tential-tech-tuesday-tickets-1366415157519",
+    ],
+    "Eventbrite Orgs":[
+        #"https://www.eventbrite.com/o/blk-tech-connect-107618183651",
+    ],
+    "Jotform":[
+        #"https://form.jotform.com/251063812795157"
+    ],
+    "Luma":[
+        #"https://luma.com/2d1a4uwv"
+    ],
+    "Luma Orgs":[
+        #"https://lu.ma/calendar/cal-vzms1nGZmYUUCrj",
+    ],
+    "Luma Users":[
+        #"usr-cTlmPNsi6jHYweP" # BioPark events
+    ],
+    "Google Forms":[
+        #"http://docs.google.com/forms/d/e/1FAIpQLSfAHwexta7vxLto1xmvBxFNawicAUtRrjTKqN0jHs25RjLCQg/viewform"
+    ]
+}
