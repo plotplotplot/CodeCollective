216179d007a774edf69d3a1eb9b0e39ca5f3148a
Julian Coy <jcloiacon@gmail.com>
2026-04-24 20:45:23 -0400

    Extract portal into OrgPortal submodule

--- portal/web/src/ui/dashboard/components/context/activity/ActivityRow.tsx
+++
@@ -1,7 +0,0 @@
-export function ActivityRow(props: { label: 'Signed' | 'Following'; value: string }) {
-  return (
-    <div className="sans" style={{ fontSize: 15, color: 'var(--text-primary)' }}>
-      <span style={{ fontWeight: 600 }}>{props.label}:</span> <span style={{ fontWeight: 400 }}>{props.value}</span>
-    </div>
-  )
-}
